f168860ef11e6388dd18b765b94293331e4e3b9a
sskangho <sskangho@naver.com>
2026-09-23 15:12:43 +0900

    fix: save all edited configuration rows

--- src/Edge/Parking.EdgeManager.Core/ConfigurationEditPolicy.cs
+++ src/Edge/Parking.EdgeManager.Core/ConfigurationEditPolicy.cs
@@ -1,7 +1,12 @@
+using System.Data;
+
 namespace Parking.EdgeManager.Core;
 
 public static class ConfigurationEditPolicy
 {
     public static bool ShouldCommit(bool isDirty, bool isCheckBox) =>
         isDirty && isCheckBox;
+
+    public static bool ShouldSave(DataRowState state) =>
+        state is DataRowState.Added or DataRowState.Modified;
 }
